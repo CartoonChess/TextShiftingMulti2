21bd169ec48b9ecdbc673f6db0d904de7b15cad6
CartoonChess <43363630+CartoonChess@users.noreply.github.com>
2023-05-28 11:57:01 +0000

    basic chat understood

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+    // io.emit -> send to all
+    // socket.broadcast.emit -> send to all but sender client
+    
+    console.log('A user has connected.');
+    //socket.broadcast.emit('chat message', 'A user has connected.');
+    socket.broadcast.emit('chat message', ({msg: 'A user has connected.', username: 'System'}));
+    
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected.');
+        socket.broadcast.emit('chat message', ({msg: 'A user has disconnected.', username: 'System'}));
+    });
+    
+    // socket.on('chat message', msg => {
+    socket.on('chat message', ({msg, user}) => {
+        //io.emit('chat message', msg);
+        socket.broadcast.emit('chat message', ({msg: msg, username: user}));
+    });
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
