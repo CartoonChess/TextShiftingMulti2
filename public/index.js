a418bc17270089adf1d878901802f22134306c77
CartoonChess <43363630+CartoonChess@users.noreply.github.com>
2023-07-06 03:55:37 +0000

    GameSocket class created; interacts with server but doesn't yet update display

--- public/index.js
+++ public/index.js
@@ -6,120 +6,120 @@ import { Player, RemotePlayer } from './js/Character.js';
 import MessageLog from './js/MessageLog.js';
 const log = new MessageLog(document.getElementById('message-log'), true);
 
-// Get player set up for remote connection
-// Using default URL param
-const socket = io(window.location.host, { autoConnect: false });
-//socket.auth = { username: "joe" };
+// // Get player set up for remote connection
+// // Using default URL param
+// const socket = io(window.location.host, { autoConnect: false });
+// //socket.auth = { username: "joe" };
 
-// `localStorage` is a property of browser `window`
-const sessionId = localStorage.getItem('sessionId');
-if (sessionId) {
-    socket.auth = { sessionId };
-    log.print(`had seshId ${sessionId}.`)
-}
+// // `localStorage` is a property of browser `window`
+// const sessionId = localStorage.getItem('sessionId');
+// if (sessionId) {
+//     socket.auth = { sessionId };
+//     log.print(`had seshId ${sessionId}.`)
+// }
 
-// Not in the tutorial but
-// let's just try connecting down here instead
-socket.connect();
+// // Not in the tutorial but
+// // let's just try connecting down here instead
+// socket.connect();
 
-// Get session ID, whether new or returning
-socket.on('session', ({ sessionId, userId }) => {
-    // 'attach sessionId to next reconnection attempts'
-    socket.auth = { sessionId };
-    // Store in browser's localStorage
-    localStorage.setItem('sessionId', sessionId);
-    // Save (public) userId
-    socket.userId = userId;
-    // socket.positionOnMap = positionOnMap;
-    log.print(`got userId ${socket.userId}.`);
-});
-
-socket.on('connect_error', (err) => {
-    var errorMessage = '(unknown error)';
-    if (err.message) { errorMessage = err.message; }
-    log.print(`ERROR: [Socket.io]: ${errorMessage}.`);
-});
-
-// socket.on('self connected', (id) => {
-//     log.print(`You're in (ID: ${id}).`);
+// // Get session ID, whether new or returning
+// socket.on('session', ({ sessionId, userId }) => {
+//     // 'attach sessionId to next reconnection attempts'
+//     socket.auth = { sessionId };
+//     // Store in browser's localStorage
+//     localStorage.setItem('sessionId', sessionId);
+//     // Save (public) userId
+//     socket.userId = userId;
+//     // socket.positionOnMap = positionOnMap;
+//     log.print(`got userId ${socket.userId}.`);
 // });
 
-var socketConnectCount = 0;
-socket.on('connect', () => {
-    socketConnectCount++;
-    log.print(`Socket connect count: ${socketConnectCount}.`);
-});
+// socket.on('connect_error', (err) => {
+//     var errorMessage = '(unknown error)';
+//     if (err.message) { errorMessage = err.message; }
+//     log.print(`ERROR: [Socket.io]: ${errorMessage}.`);
+// });
 
-var socketDisonnectCount = 0;
-socket.on('disconnect', (reason) => {
-    socketDisonnectCount++;
-    log.print(`Socket disconnect count: ${socketDisonnectCount}. Reason: ${reason}.`);
-});
+// // socket.on('self connected', (id) => {
+// //     log.print(`You're in (ID: ${id}).`);
+// // });
 
-// Get already-connected users when joining
-// socket.on('users'...
-socket.on('all players', (allPlayers) => {
-    // Let's just replace the old data and get in sync w/ server
-    remotePlayers.length = 0;
-    allPlayers.forEach((json) => {
-        const remotePlayer = RemotePlayer.fromJson(json);
-        log.print(`found player (id ${remotePlayer.id}, position ${remotePlayer.position}`);
-        // Only add if it's not ourself
-        if (remotePlayer.id !== socket.userId) {
-            remotePlayers.push(remotePlayer);
-        }
-    });
-    log.print(`number of remote players: ${remotePlayers.length}`);
-    updateText();
-});
+// var socketConnectCount = 0;
+// socket.on('connect', () => {
+//     socketConnectCount++;
+//     log.print(`Socket connect count: ${socketConnectCount}.`);
+// });
 
-// Get new users who join after you
-// socket.on('user connected'...
-// socket.on('other connected', ({ userId, positionOnMap }) => {
-socket.on('other connected', (remotePlayerJson) => {
-    const remotePlayer = RemotePlayer.fromJson(remotePlayerJson);
-    log.print(`Friend's in (ID: ${remotePlayer.id}).`);
-    if (remotePlayer.id === socket.userId) { return; }
-    // Only add if it's a new player, not a second session
-    for (const existingPlayer of remotePlayers) {
-        if (remotePlayer.id === existingPlayer.id) { return; }
-    }
-    remotePlayers.push(remotePlayer);
-});
+// var socketDisonnectCount = 0;
+// socket.on('disconnect', (reason) => {
+//     socketDisonnectCount++;
+//     log.print(`Socket disconnect count: ${socketDisonnectCount}. Reason: ${reason}.`);
+// });
 
-// Only happens when remote user ends all sessions
-socket.on('other disconnected', (userId) => {
-    log.print(`userId ${userId} left.`);
-    for (let i = 0; i < remotePlayers.length; i++) {
-        if (remotePlayers[i].id === userId) {
-            remotePlayers.splice(i, 1);
-        }
-    }
-    updateText();
-});
+// // Get already-connected users when joining
+// // socket.on('users'...
+// socket.on('all players', (allPlayers) => {
+//     // Let's just replace the old data and get in sync w/ server
+//     remotePlayers.length = 0;
+//     allPlayers.forEach((json) => {
+//         const remotePlayer = RemotePlayer.fromJson(json);
+//         log.print(`found player (id ${remotePlayer.id}, position ${remotePlayer.position}`);
+//         // Only add if it's not ourself
+//         if (remotePlayer.id !== socket.userId) {
+//             remotePlayers.push(remotePlayer);
+//         }
+//     });
+//     log.print(`number of remote players: ${remotePlayers.length}`);
+//     updateText();
+// });
 
-// socket.on('private message'...
-socket.on('move', ({ userId, positionOnMap }) => {
-    const position = Coordinate.fromJson(positionOnMap);
+// // Get new users who join after you
+// // socket.on('user connected'...
+// // socket.on('other connected', ({ userId, positionOnMap }) => {
+// socket.on('other connected', (remotePlayerJson) => {
+//     const remotePlayer = RemotePlayer.fromJson(remotePlayerJson);
+//     log.print(`Friend's in (ID: ${remotePlayer.id}).`);
+//     if (remotePlayer.id === socket.userId) { return; }
+//     // Only add if it's a new player, not a second session
+//     for (const existingPlayer of remotePlayers) {
+//         if (remotePlayer.id === existingPlayer.id) { return; }
+//     }
+//     remotePlayers.push(remotePlayer);
+// });
+
+// // Only happens when remote user ends all sessions
+// socket.on('other disconnected', (userId) => {
+//     log.print(`userId ${userId} left.`);
+//     for (let i = 0; i < remotePlayers.length; i++) {
+//         if (remotePlayers[i].id === userId) {
+//             remotePlayers.splice(i, 1);
+//         }
+//     }
+//     updateText();
+// });
+
+// // socket.on('private message'...
+// socket.on('move', ({ userId, positionOnMap }) => {
+//     const position = Coordinate.fromJson(positionOnMap);
     
-    // If you moved in one tab, update all your tabs
-    if (userId === socket.userId) {
-        player.position = position;
-        return updateText();
-    }
+//     // If you moved in one tab, update all your tabs
+//     if (userId === socket.userId) {
+//         player.position = position;
+//         return updateText();
+//     }
 
-    // Otherwise, let's see which remote user moved
-    for (let i = 0; i < remotePlayers.length; i++) {
-        if (remotePlayers[i].id === userId) {
-            remotePlayers[i].position = position;
-            const isInView = view.isVisible(remotePlayers[i]);
-            if (remotePlayers[i].wasInView || isInView) {
-                updateText();
-            }
-            remotePlayers[i].wasInView = isInView;
-        }
-    }
-});
+//     // Otherwise, let's see which remote user moved
+//     for (let i = 0; i < remotePlayers.length; i++) {
+//         if (remotePlayers[i].id === userId) {
+//             remotePlayers[i].position = position;
+//             const isInView = view.isVisible(remotePlayers[i]);
+//             if (remotePlayers[i].wasInView || isInView) {
+//                 updateText();
+//             }
+//             remotePlayers[i].wasInView = isInView;
+//         }
+//     }
+// });
 
 // Map and view bounds
 const map = new Map(29, 29);
@@ -180,6 +180,10 @@ const arrays = generateArrays(map.width, map.height);
 const player = new Player();
 player.position = map.center;
 const remotePlayers = [];
+
+import GameSocket from './js/GameSocket.js';
+const socket = new GameSocket(log, view, player, remotePlayers);
+socket.listen();
 
 import { RandomBytes } from './randomBytes.js';
 
@@ -304,18 +308,20 @@ function moveIfAble(character, direction) {
     }
 }
 
+// TODO: Move these into GameSocket maybe
+
 // Send position to other players
 function broadcastMove() {
-    // Should this be broadcast/other instead? Can it be?
-    socket.emit('move', player.position.toJson());
+    // // Should this be broadcast/other instead? Can it be?
+    // socket.emit('move', player.position.toJson());
 }
 
 function pingServer() {
-    const start = Date.now();
-    socket.emit('latency', () => {
-        const duration = Date.now() - start;
-        return duration;
-    });
+    // const start = Date.now();
+    // socket.emit('latency', () => {
+    //     const duration = Date.now() - start;
+    //     return duration;
+    // });
 }
 
 document.addEventListener('keydown', function (event) {
---
+++ public/js/GameSocket.js
@@ -0,0 +1,142 @@
+import { Coordinate } from './Map.js';
+import { RemotePlayer } from './Character.js';
+
+export default class GameSocket {
+    #socket;
+    // #sessionId;
+    // #connectCount = 0;
+    // #disconnectCount = 0;
+
+    #log;
+    #view;
+    #player;
+    #remotePlayers;
+    
+    // constructor(window, log, player, remotePlayers, view) {
+    constructor(log, view, player, remotePlayers) {
+        // NEW: refs to other vars
+        this.log = log;
+        this.view = view;
+        this.player = player;
+        this.remotePlayers = remotePlayers;
+        
+        // Get player set up for remote connection
+        // Using default URL param
+        this.socket = io(window.location.host, { autoConnect: false });
+        //socket.auth = { username: "joe" };
+        
+        // `localStorage` is a property of browser `window`
+        const sessionId = localStorage.getItem('sessionId');
+        if (sessionId) {
+            this.socket.auth = { sessionId };
+            this.log.print(`had seshId ${sessionId}.`)
+        }
+        
+        // Not in the tutorial but
+        // let's just try connecting down here instead
+        this.socket.connect();
+    }
+
+    listen() {
+        // Get session ID, whether new or returning
+        this.socket.on('session', ({ sessionId, userId }) => {
+            // 'attach sessionId to next reconnection attempts'
+            this.socket.auth = { sessionId };
+            // Store in browser's localStorage
+            localStorage.setItem('sessionId', sessionId);
+            // Save (public) userId
+            this.socket.userId = userId;
+            // socket.positionOnMap = positionOnMap;
+            this.log.print(`got userId ${this.socket.userId}.`);
+        });
+        
+        this.socket.on('connect_error', (err) => {
+            var errorMessage = '(unknown error)';
+            if (err.message) { errorMessage = err.message; }
+            this.log.print(`ERROR: [Socket.io]: ${errorMessage}.`);
+        });
+        
+        // this.socket.on('self connected', (id) => {
+        //     this.log.print(`You're in (ID: ${id}).`);
+        // });
+        
+        // var socketConnectCount = 0;
+        let connectCount = 0;
+        this.socket.on('connect', () => {
+            connectCount++;
+            this.log.print(`Socket connect count: ${connectCount}.`);
+        });
+        
+        // var socketDisonnectCount = 0;
+        let disconnectCount = 0;
+        this.socket.on('disconnect', (reason) => {
+            disconnectCount++;
+            this.log.print(`Socket disconnect count: ${disconnectCount}. Reason: ${reason}.`);
+        });
+        
+        // Get already-connected users when joining
+        // socket.on('users'...
+        this.socket.on('all players', (allPlayers) => {
+            // Let's just replace the old data and get in sync w/ server
+            this.remotePlayers.length = 0;
+            allPlayers.forEach((json) => {
+                const remotePlayer = RemotePlayer.fromJson(json);
+                this.log.print(`found player (id ${remotePlayer.id}, position ${remotePlayer.position}`);
+                // Only add if it's not ourself
+                if (remotePlayer.id !== this.socket.userId) {
+                    this.remotePlayers.push(remotePlayer);
+                }
+            });
+            this.log.print(`number of remote players: ${this.remotePlayers.length}`);
+            updateText();
+        });
+        
+        // Get new users who join after you
+        // socket.on('user connected'...
+        // socket.on('other connected', ({ userId, positionOnMap }) => {
+        this.socket.on('other connected', (remotePlayerJson) => {
+            const remotePlayer = RemotePlayer.fromJson(remotePlayerJson);
+            this.log.print(`Friend's in (ID: ${remotePlayer.id}).`);
+            if (remotePlayer.id === this.socket.userId) { return; }
+            // Only add if it's a new player, not a second session
+            for (const existingPlayer of this.remotePlayers) {
+                if (remotePlayer.id === existingPlayer.id) { return; }
+            }
+            this.remotePlayers.push(remotePlayer);
+        });
+        
+        // Only happens when remote user ends all sessions
+        this.socket.on('other disconnected', (userId) => {
+            this.log.print(`userId ${userId} left.`);
+            for (let i = 0; i < this.remotePlayers.length; i++) {
+                if (this.remotePlayers[i].id === userId) {
+                    this.remotePlayers.splice(i, 1);
+                }
+            }
+            updateText();
+        });
+        
+        // socket.on('private message'...
+        this.socket.on('move', ({ userId, positionOnMap }) => {
+            const position = Coordinate.fromJson(positionOnMap);
+            
+            // If you moved in one tab, update all your tabs
+            if (userId === this.socket.userId) {
+                this.player.position = position;
+                return updateText();
+            }
+        
+            // Otherwise, let's see which remote user moved
+            for (let i = 0; i < this.remotePlayers.length; i++) {
+                if (this.remotePlayers[i].id === userId) {
+                    this.remotePlayers[i].position = position;
+                    const isInView = this.view.isVisible(this.remotePlayers[i]);
+                    if (this.remotePlayers[i].wasInView || isInView) {
+                        updateText();
+                    }
+                    this.remotePlayers[i].wasInView = isInView;
+                }
+            }
+        });
+    }
+}
