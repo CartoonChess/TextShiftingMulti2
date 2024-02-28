b151f41c62650f2edacec5d1cb2c245a625eaad5
CartoonChess <philip@hkpp.fyi>
2024-02-28 18:42:56 +0900

    increase express filesize for bigger maps

--- index.js
+++ index.js
@@ -63,7 +63,11 @@ const publicDir = path.join(__dirname, 'public');
 app.use('/', express.static(publicDir));
 
 // Let express parse json POSTs
-app.use(express.json());
+// app.use(express.json());
+// Increase acceptable filesize/URL encoded strings
+// TODO: Use gzip (JS now has CompressionStreams for client side)
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // Provide maps directory listing for editor mode
 // Gives name, or name with relative path if in subdirectory
