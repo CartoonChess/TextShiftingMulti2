5677bfad3dc206f7e368d90b5a10712d60c002b1
CartoonChess <philip@hkpp.fyi>
2024-03-03 15:42:53 +0900

    Tile ts to js

--- public/js/Tile.ts
+++ public/js/Tile.ts
@@ -11,13 +11,20 @@
 //     }
 // }
 
-export class WarpTileScript {
-    constructor(destinationCoordinate, destinationMap) {
+import { Coordinate } from "./GameMap.js"
+
+interface TileScript {}
+
+export class WarpTileScript implements TileScript {
+    destinationCoordinate: Coordinate
+    destinationMap?: string
+
+    constructor(destinationCoordinate: Coordinate, destinationMap?: string) {
         if (typeof destinationCoordinate === 'string') {
-            return console.error('WarpTileScript must take at least one of two arguments, in order: a Coordinate and a string representing a map package name.');
+            throw new Error('WarpTileScript must take at least one of two arguments, in order: a Coordinate and a string representing a map package name.')
         }
-        this.destinationCoordinate = destinationCoordinate;
-        this.destinationMap = destinationMap;
+        this.destinationCoordinate = destinationCoordinate
+        this.destinationMap = destinationMap
     }
 
     // TODO: Should some warping logic be handled here?
@@ -26,31 +33,33 @@ export class WarpTileScript {
 export default class Tile {
     //parent/derivedFrom?/"name/type/class"? for resuable tiles...?
     //id = Symbol(); // prob shouldn't do random, or else we can't refer to it in map design scripts
-    symbol = ' '; //if type needed: GameSymbol
-    isVisible = true;
-    isSolid = false;
+    symbol = ' ' //if type needed: GameSymbol
+    isVisible? = true
+    isSolid? = false
     //type = new Character(); //(collectable) item, (furniture-like) foreground?, background/solid...
-    color = 'inherit'; // default (like black)
+    color? = 'inherit' // default (like black)
     // TODO: Better just to have View.updateView() strip `.style` properties when undefined?
     // backgroundColor = 0x00ff00; //can be null (transparent)
-    backgroundColor = 'inherit';
+    backgroundColor? = 'inherit'
     // alpha/transparency (0.00 ~ 1?)
-    image;
+    // image
     //position = new Coordinate(0, 0);
-    layer = 0; //or add Z to Coordinate
+    layer? = 0 //or add Z to Coordinate
     // [facing]Direction = Direction.Down;
     // "action/trigger/script" -> for example, as an NPC, or a warp point
 
+    // TODO: TileScript interface?
+    scripts?: TileScript[]
     // #scripts;
     // _scripts;
 
-    constructor(json) {
-        if (!json) { return; }
-        if (json.symbol) { this.symbol = json.symbol };
-        if (json.isSolid) { this.isSolid = json.isSolid };
-        if (json.color) { this.color = json.color };
-        if (json.backgroundColor) { this.backgroundColor = json.backgroundColor };
-        this.scripts = json.scripts;
+    constructor(json: { symbol: string; isSolid: boolean | undefined; color: string | undefined; backgroundColor: string | undefined; scripts: TileScript[] | undefined }) {
+        if (!json) { return }
+        if (json.symbol) { this.symbol = json.symbol }
+        if (json.isSolid) { this.isSolid = json.isSolid }
+        if (json.color) { this.color = json.color }
+        if (json.backgroundColor) { this.backgroundColor = json.backgroundColor }
+        this.scripts = json.scripts
     }
 
     // get scripts() {
